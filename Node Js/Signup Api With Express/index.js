9196d8bdef0c8686b6f7acec6c14174945d13bf1
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-01-18 15:00:11 +0500

    Learn Middleware Functions  And how to use it in this code
    Middleware function :
    There  are two types
    
    1 - Global Middleware func
    2 - path specific middleware
    
    middleware function provide extra functionality  .

--- Node Js/Signup Api With Express/index.js
+++ Node Js/Signup Api With Express/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8000;
 
 //middleware 
-app.use(express.json());
+app.use(express.json());  // Global Middleware 
 
 
 //mini app 
@@ -13,12 +13,28 @@ const authReducer = express.Router();
 // baser url 
 app.use('/auth',authReducer);
 
-authReducer.route('/signup')
-.get(getSingup)
+authReducer.route('/signup')    // Path spceific Middleware 
+.get(middleware1, getSingup,middleware2)
 .post(postSignup)
 
-function getSingup(req,res){
-  res.sendFile('./views/signup.html',{root:__dirname});
+
+// Middleware Function 
+function middleware1(req,res,next){
+  console.log("Middleware Function encounter");
+  next();
+}
+
+
+function middleware2(req,res,next){
+     console.log("Middleware2 is encountered");
+     res.sendFile('./views/signup.html',{root:__dirname});
+     console.log("Req and Res Cycle is ended !!");
+}
+
+function getSingup(req,res,next){
+  console.log("getSignup Call");
+ 
+  next();
 }
 
 function postSignup(req,res){
