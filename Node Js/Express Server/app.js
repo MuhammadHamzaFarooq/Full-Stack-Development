f17b207f832fb9c028c3976af712bbb53ebb216a
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-02-04 18:18:37 +0500

    update express server

--- Node Js/Express Server/app.js
+++ Node Js/Express Server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    // res.send("Welcome in Express Server");
+    res.send("Welcome in Express Server");
     res.sendFile('./views/index.html',{root:__dirname});
 });
 
